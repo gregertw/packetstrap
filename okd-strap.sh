d70271be8013de7f85742503e3e011c556850419
Greger Wedel <greger.wedel@cognite.com>
2020-10-17 08:00:18 +0200

    First pass at OKD fixes

---
+++ okd-strap.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# This simple bash scripts will do just about everything needed to have a helper node for deployment of OKD
+#  
+# You need to:
+#   - Pass your domain name as $1
+#   - Pass your subdomain name (and cluster name) as $2
+# For example: ./packetstrap.sh demonstr8.net test
+
+# You also need to:
+#   - have a ssh key in ~/.ssh/id_rsa.pub 
+
+[[ $# -ne 2 ]] && echo "Please provide 2 arguments" && exit 254
+
+# Create SSH key
+ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+
+echo "==== Setting variables"
+# Pullsecret for OKD
+PULLSECRET="{\"auths\":{\"fake\":{\"auth\": \"bar\"}}}"
+BASEDOMAIN=$1
+METADATANAME=$2
+SSHKEY=`cat ~/.ssh/id_rsa.pub`
+PUBLICIP=`ip address show dev bond0 |grep bond0 |grep -v bond0:0 |grep inet |awk -F" " '{ print $2}' |awk -F"/" '{print $1}'`
+
+echo "==== create manifests"
+mkdir packetinstall
+cat <<EOT > packetinstall/install-config.yaml
+apiVersion: v1
+baseDomain: BASEDOMAIN
+compute:
+- hyperthreading: Enabled   
+  name: worker
+  replicas: 0 
+controlPlane:
+  hyperthreading: Enabled   
+  name: master 
+  replicas: 3 
+metadata:
+  name: METADATANAME
+networking:
+  clusterNetwork:
+  - cidr: 10.128.0.0/14 
+    hostPrefix: 23 
+  networkType: OpenShiftSDN
+  serviceNetwork: 
+  - 172.30.0.0/16
+platform:
+  none: {} 
+fips: false 
+pullSecret: 'PULLSECRET'
+sshKey: 'SSHKEY'
+EOT
+
+sed -i "s/BASEDOMAIN/${BASEDOMAIN}/" packetinstall/install-config.yaml
+sed -i "s/METADATANAME/${METADATANAME}/" packetinstall/install-config.yaml
+sed -i "s%PULLSECRET%${PULLSECRET}%" packetinstall/install-config.yaml
+sed -i "s%SSHKEY%${SSHKEY}%" packetinstall/install-config.yaml
+
+./openshift-install create manifests --dir=packetinstall
+
+sed -i 's/mastersSchedulable: true/mastersSchedulable: false/' packetinstall/manifests/cluster-scheduler-02-config.yml
+
+./openshift-install create ignition-configs --dir=packetinstall
+
+
+echo "==== Create publicly accessible directory, Copy ignition files, Create iPXE files"
+
+mkdir /var/www/html/packetstrap
+cp packetinstall/*.ign /var/www/html/packetstrap/
+cp ./binaries/pxe/* /var/www/html/packetstrap/
+chmod 644 /var/www/html/packetstrap/*.ign
+
+
+# Set up bootstrap with fedora-coreos.initramfs.x86_64.img, fedora-coreos.x86_64.iso, fedora-coreos.kernel-x86_64, fedora-coreos.rootfs.x86_64.img, 
+# fedora-coreos.metal.x86_64.raw.xz
+cat <<EOT > /var/www/html/packetstrap/bootstrap.boot
+#!ipxe
+
+kernel http://PUBLICIP:8080/packetstrap/fedora-coreos.kernel-x86_64 ip=dhcp rd.neednet=1 initrd=fedora-coreos.initramfs.x86_64.img console=ttyS1,115200n8 coreos.inst=yes coreos.inst.install_dev=sda coreos.inst.image_url=http://PUBLICIP:8080/packetstrap/fedora-coreos.metal.x86_64.raw.xz coreos.inst.ignition_url=http://PUBLICIP:8080/packetstrap/bootstrap.ign coreos.live.rootfs_url=fedora-coreos.rootfs.x86_64.img
+initrd http://PUBLICIP:8080/packetstrap/fedora-coreos.initramfs.x86_64.img
+boot
+EOT
+
+cat <<EOT > /var/www/html/packetstrap/master.boot
+#!ipxe
+
+kernel http://PUBLICIP:8080/packetstrap/fedora-coreos.kernel-x86_64 ip=dhcp rd.neednet=1 initrd=fedora-coreos.initramfs.x86_64.img console=ttyS1,115200n8 coreos.inst=yes coreos.inst.install_dev=sda coreos.inst.image_url=http://PUBLICIP:8080/packetstrap/fedora-coreos.metal.x86_64.raw.xz coreos.inst.ignition_url=http://PUBLICIP:8080/packetstrap/master.ign coreos.live.rootfs_url=fedora-coreos.rootfs.x86_64.img
+initrd http://PUBLICIP:8080/packetstrap/fedora-coreos.initramfs.x86_64.img
+boot
+EOT
+
+cat <<EOT > /var/www/html/packetstrap/worker.boot
+#!ipxe
+
+kernel http://PUBLICIP:8080/packetstrap/fedora-coreos.kernel-x86_64 ip=dhcp rd.neednet=1 initrd=fedora-coreos.initramfs.x86_64.img console=ttyS1,115200n8 coreos.inst=yes coreos.inst.install_dev=sda coreos.inst.image_url=http://PUBLICIP:8080/packetstrap/fedora-coreos.metal.x86_64.raw.xz coreos.inst.ignition_url=http://PUBLICIP:8080/packetstrap/worker.ign coreos.live.rootfs_url=fedora-coreos.rootfs.x86_64.img
+initrd http://PUBLICIP:8080/packetstrap/fedora-coreos.initramfs.x86_64.img
+boot
+EOT
+
+
+sed -i "s/PUBLICIP/$PUBLICIP/g" /var/www/html/packetstrap/bootstrap.boot
+sed -i "s/PUBLICIP/$PUBLICIP/g" /var/www/html/packetstrap/master.boot
+sed -i "s/PUBLICIP/$PUBLICIP/g" /var/www/html/packetstrap/worker.boot
+
+
+
+echo "==== all done, you can now iPXE servers to:"
+echo "       http://${PUBLICIP}:8080/packetstrap/bootstrap.boot" | tee -a iPXE_info.txt
+echo "       http://${PUBLICIP}:8080/packetstrap/master.boot" | tee -a iPXE_info.txt
+echo "       http://${PUBLICIP}:8080/packetstrap/worker.boot" | tee -a iPXE_info.txt
+
+echo "==== setting path"
+export PATH=$PATH:$PWD
+
+
+
---
+++ os-strap.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+# This simple bash scripts will bootstrap a CentOS7 with OS installs in prep for OKD install
+#  
+
+# You also need to:
+#   - have a ssh key in ~/.ssh/id_rsa.pub 
+
+[[ $# -ne 2 ]] && echo "Please provide 2 arguments" && exit 254
+
+# Create SSH key
+ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+
+echo "==== Setting variables"
+SSHKEY=`cat ~/.ssh/id_rsa.pub`
+PUBLICIP=`ip address show dev bond0 |grep bond0 |grep -v bond0:0 |grep inet |awk -F" " '{ print $2}' |awk -F"/" '{print $1}'`
+
+echo "==== clean up yum and repo setup"
+yum clean all
+yum install yum-utils -y
+sudo yum install rhel-7-server-extras-rpms rhel-7-server-optional-rpms
+
+echo "==== setup firewall"
+firewall-cmd --add-port=80/tcp
+firewall-cmd --add-port=443/tcp
+firewall-cmd --add-port=8080/tcp
+firewall-cmd --add-port=8088/tcp
+firewall-cmd --add-port=6443/tcp
+firewall-cmd --add-port=22623/tcp
+firewall-cmd --add-port=2376/tcp
+firewall-cmd --add-port=9000/tcp
+firewall-cmd --add-port=2376/udp
+firewall-cmd --add-port=111/tcp
+firewall-cmd --add-port=662/tcp
+firewall-cmd --add-port=875/tcp
+firewall-cmd --add-port=892/tcp
+firewall-cmd --add-port=2049/tcp
+firewall-cmd --add-port=32803/tcp
+firewall-cmd --add-port=111/udp
+firewall-cmd --add-port=662/udp
+firewall-cmd --add-port=875/udp
+firewall-cmd --add-port=892/udp
+firewall-cmd --add-port=2049/udp
+firewall-cmd --add-port=32803/udp
+firewall-cmd --runtime-to-permanent
+
+echo "==== install and configure nfs"
+yum install nfs-utils -y
+echo "/mnt/data *(rw,sync,no_wdelay,no_root_squash,insecure)" >> /etc/exports
+#This is a terrible idea
+chmod -R 777 /mnt/data
+
+mkdir /mnt/data
+service nfs start
+exportfs
+
+echo "==== install and configure haproxy"
+yum install haproxy -y
+cat <<EOT > /etc/haproxy/haproxy.cfg
+defaults
+	mode                	http
+	log                 	global
+	option              	httplog
+	option              	dontlognull
+	option forwardfor   	except 127.0.0.0/8
+	option              	redispatch
+	retries             	3
+	timeout http-request	10s
+	timeout queue       	1m
+	timeout connect     	10s
+	timeout client      	300s
+	timeout server      	300s
+	timeout http-keep-alive 10s
+	timeout check       	10s
+	maxconn             	20000
+
+# Useful for debugging, dangerous for production
+listen stats
+	bind :9000
+	mode http
+	stats enable
+	stats uri /
+
+frontend openshift-api-server
+	bind *:6443
+	default_backend openshift-api-server
+	mode tcp
+	option tcplog
+
+backend openshift-api-server
+	balance source
+	mode tcp
+	server master-0 MASTER0IP:6443 check
+	server master-1 MASTER1IP:6443 check
+	server master-2 MASTER2IP:6443 check
+        server bootstrap BOOTSTRAPIP:6443 check
+
+frontend machine-config-server
+	bind *:22623
+	default_backend machine-config-server
+	mode tcp
+	option tcplog
+
+backend machine-config-server
+	balance source
+	mode tcp
+        server master-0 MASTER0IP:22623 check
+        server master-1 MASTER1IP:22623 check
+        server master-2 MASTER2IP:22623 check
+        server bootstrap BOOTSTRAPIP:22623 check
+
+frontend ingress-http
+	bind *:80
+	default_backend ingress-http
+	mode tcp
+	option tcplog
+
+backend ingress-http
+	balance source
+	mode tcp
+	server worker-0 WORKER0IP:80 check
+	server worker-1 WORKER1IP:80 check
+
+frontend ingress-https
+	bind *:443
+	default_backend ingress-https
+	mode tcp
+	option tcplog
+
+backend ingress-https
+	balance source
+	mode tcp
+	server worker-0 WORKER0IP:443 check
+	server worker-1 WORKER1IP:443 check
+EOT
+
+
+echo "==== install and configure apache"
+yum install httpd -y
+sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/httpd.conf
+echo "apache is setup" > /var/www/html/test
+service httpd start
+
+echo "==== get okd install, client, and COS images"
+pushd
+mkdir binaries
+cd binaries
+wget https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-10-03-012432/openshift-client-linux-4.5.0-0.okd-2020-10-03-012432.tar.gz
+wget https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-10-03-012432/openshift-install-linux-4.5.0-0.okd-2020-10-03-012432.tar.gz
+
+mkdir pxe
+pushd
+cd pxe
+# Get fedora-coreos.initramfs.x86_64.img, fedora-coreos.x86_64.iso, fedora-coreos.kernel-x86_64, fedora-coreos.rootfs.x86_64.img, 
+# fedora-coreos.metal.x86_64.raw.xz
+wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20200923.3.0/x86_64/fedora-coreos-32.20200923.3.0-live-initramfs.x86_64.img
+mv fedora-coreos-32.20200923.3.0-live-initramfs.x86_64.img fedora-coreos.initramfs.x86_64.img
+wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20200923.3.0/x86_64/fedora-coreos-32.20200923.3.0-live.x86_64.iso
+mv fedora-coreos-32.20200923.3.0-live.x86_64.iso fedora-coreos.x86_64.iso
+wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20200923.3.0/x86_64/fedora-coreos-32.20200923.3.0-live-kernel-x86_64
+mv fedora-coreos-32.20200923.3.0-live-kernel-x86_64 fedora-coreos.kernel-x86_64
+# CoreOS now requires rootfs image to boot
+wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20200923.3.0/x86_64/fedora-coreos-32.20200923.3.0-live-rootfs.x86_64.img
+mv fedora-coreos-32.20200923.3.0-live-rootfs.x86_64.img fedora-coreos.rootfs.x86_64.img
+wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20200923.3.0/x86_64/fedora-coreos-32.20200923.3.0-metal.x86_64.raw.xz
+mv fedora-coreos-32.20200923.3.0-metal.x86_64.raw.xz fedora-coreos.metal.x86_64.raw.xz
+
+popd
+popd
+
+tar -xvzf ./binaries/openshift-install-linux.tar.gz
+tar -xvzf ./binaries/openshift-client-linux.tar.gz
+
+echo "OS upgrades, firewall, haproxy, and apache, as well as OKD packages installed."
